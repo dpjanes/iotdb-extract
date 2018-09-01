d16a48614f2d8d639a1751add86e3560af09b90d
David Janes <david@consensas.com>
2018-09-01 17:21:31 -0400

    mostly boilerplate

---
+++ logger.js
@@ -0,0 +1,39 @@
+/*
+ *  logger.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2018-09-01
+ *
+ *  Copyright [2013-2018] [David P. Janes]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+
+const path = require("path")
+
+const _root = path.dirname(path.dirname(__filename))
+
+const logger = (source) => _.logger.make({
+    name: "iotdb-extract",
+    source: source.substring(_root.length + 1).replace(/[.]js$/, ""),
+})
+
+/**
+ *  API
+ */
+module.exports = logger
