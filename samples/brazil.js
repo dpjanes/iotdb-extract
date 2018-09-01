a0a2d1ea94d852c22eab093bf8f652da37ff61df
David Janes <david@consensas.com>
2018-09-01 19:03:27 -0400

    and entities and sentiment

--- samples/brazil.js
+++ samples/brazil.js
@@ -9,16 +9,32 @@
 const _ = require("iotdb-helpers")
 const extract = require("iotdb-extract")
 const fs = require("iotdb-fs")
+const aws = require("iotdb-awslib")
 
 _.promise.make({
-    url: "https://www.bbc.com/news/world-latin-america-45380237"
+    // awsd: awsd,
+    // url: 
 })
-    .then(fs.read.p("brazil.html", "utf-8"))
+    // aws setup
+    .then(_.promise.add("awsd", require("./aws.json")))
+    .then(aws.initialize)
+    .then(aws.comprehend.initialize)
 
+    // extract
     .then(extract.initialize)
     .then(extract.load_rules.builtin)
-    .then(extract.find)
+
+    // find the rule
+    .then(extract.find.p("https://www.bbc.com/news/world-latin-america-45380237"))
+
+    // parse the document
+    .then(fs.read.p("brazil.html", "utf-8"))
     .then(extract.extract)
+
+    // get entities & sentiment
+    .then(extract.entities)
+    .then(extract.sentiment)
+
     .then(_.promise.make(sd => {
         console.log(JSON.stringify(sd.jsons, null, 2))
     }))
