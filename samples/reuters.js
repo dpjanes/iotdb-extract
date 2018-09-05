0f2a6f4f0d50e46b2470f75bc80621daf68d0a93
David Janes <david@consensas.com>
2018-09-05 08:50:35 -0400

    slight change to rule definitions

---
+++ samples/reuters.js
@@ -0,0 +1,56 @@
+/**
+ *  reuters.js
+ *
+ *  David Janes
+ *  Consensas
+ *  2018-09-01
+ *
+ *  Copyright [2013-2018] [David P. Janes]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const _ = require("iotdb-helpers")
+const extract = require("iotdb-extract")
+const fs = require("iotdb-fs")
+const aws = require("iotdb-awslib")
+
+_.promise.make({
+    url: "https://www.reuters.com/article/us-zimbabwe-congo-rhinos/zimbabwe-to-donate-10-white-rhinos-to-congo-idUSKCN1LJ1IF",
+})
+    // aws setup
+    .then(_.promise.add("awsd", require("./aws.json")))
+    .then(aws.initialize)
+    .then(aws.comprehend.initialize)
+
+    // extract
+    .then(extract.initialize)
+    .then(extract.load_rules.builtin)
+
+    // find the rule, fetch
+    .then(extract.find)
+    .then(extract.fetch)
+
+    // analyze
+    .then(extract.extract)
+    .then(extract.entities)
+
+    .then(_.promise.make(sd => {
+        console.log(JSON.stringify(sd.jsons, null, 2))
+    }))
+    .catch(error => {
+        console.log("#", _.error.message(error))
+
+        delete error.self
+        console.log(error)
+    })
