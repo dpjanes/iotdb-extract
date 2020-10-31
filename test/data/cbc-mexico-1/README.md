ea3409d4526a4100e2d5c258b7737bf1e7dfb0c7
David Janes <david@consensas.com>
2020-10-31 16:46:59 -0400

    add debugging

---
+++ test/data/cbc-mexico-1/README.md
@@ -0,0 +1,2 @@
+Note CBC is broken because it's putting the content Javascript
+because reasons
--- test/extract.js
+++ test/extract.js
@@ -31,31 +31,32 @@ const path = require("path")
 const extract = require("..")
 const _util = require("./_util")
 
+const WRITE = process.env.WRITE === "1"
+const DUMP = process.env.DUMP === "1"
+
 describe("data", function() {
     let self = {}
 
     before(function(done) {
-        _.promise.make(self)
-           .then(_util.initialize)
+        _.promise(self)
+            .then(extract.initialize)
             .then(extract.load_rules.builtin)
-            .then(_.promise.make(sd => {
+            .make(sd => {
                 self = sd
-            }))
-            .then(_.promise.done(done))
-            .catch(done)
+                self.verbose = true
+            })
+            .end(done, {})
     })
 
     describe("good", function() {
         it("works", function(done) {
             const _test = _.promise((self, done) => {
-                const root = self.path
+                const root = path.join(__dirname, "data", self.path)
 
                 _.promise(self)
-                    .add("json", null)
                     .add("path", sd => path.join(root, "data.yaml"))
-                    .then(_.promise.optional(fs.read.json.magic))
-                    .then(_.promise.bail.conditional(sd => !sd.json))
-                    .then(_.promise.log("test", "path"))
+                    .then(fs.read.json.magic)
+                    .log("test", "path")
                     .add("json:configuration")
 
                     // find the rule
@@ -63,33 +64,40 @@ describe("data", function() {
                     .then(extract.find)
 
                     // parse the document
-                    .add("path", sd => path.join(root, "input.html"))
-                    .then(fs.read.utf8)
+                    .then(fs.read.utf8.p(path.join(root, "input.html")))
                     .then(extract.extract)
                     .then(extract.clean)
                     .add("jsons:got")
 
                     // read the expected value
                     .add("path", sd => path.join(root, "output.yaml"))
+                    .add("jsons:json")
+                    .conditional(WRITE, fs.write.yaml)
                     .then(fs.read.json.magic)
                     .add("json:expected")
 
                     // last step
                     .make(sd => {
+                        if (DUMP) {
+                            console.log(sd.got)
+                        }
+
                         assert.deepEqual(sd.got, sd.expected)
                     })
-                    .catch(_.promise.unbail)
                     .end(done, self)
             })
 
             _.promise(self)
-                .add("path", path.join(__dirname, "data"))
-                .then(fs.list)
+                .add("paths", [
+                    "bbc-brazil-1",
+                    "cbc-mexico-1",
+                    "reuters-congo-1",
+                ])
                 .each({
                     method: _test,
                     inputs: "paths:path",
                 })
-                .end(done)
+                .end(done, {})
         })
     })
 })
