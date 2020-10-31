cb12874a71c5be5ffb9a583585e1de96ee3fae20
David Janes <david@consensas.com>
2020-10-31 17:40:36 -0400

    analyze (a work in progress)

---
+++ samples/analyze.js
@@ -0,0 +1,52 @@
+/**
+ *  samples/analyze.js
+ *
+ *  David Janes
+ *  Consensas
+ *  2020-10-31
+ *  🎃🕸👻
+ *
+ *  Copyright (2013-2020) David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const _ = require("iotdb-helpers")
+const extract = require("iotdb-extract")
+const fs = require("iotdb-fs")
+const aws = require("iotdb-awslib")
+const cache = require("iotdb-cache")
+
+const path = require("path")
+
+_.promise({
+    cache$cfg: {
+        path: path.join(__dirname, "..", ".fs-cache"),
+    },
+})
+    // extract
+    .then(extract.initialize)
+
+    // parse the document and guess it's structure
+    .then(fs.read.utf8.p(path.join(__dirname, "../test/data/bbc-brazil-1/input.html")))
+    .then(extract.analyze)
+
+    .make(sd => {
+        console.log(JSON.stringify(sd.rule, null, 2))
+    })
+    .catch(error => {
+        console.log("#", _.error.message(error))
+
+        delete error.self
+        console.log(error)
+    })
